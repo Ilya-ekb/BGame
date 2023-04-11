616f3efc3db931bbecdc0e94732450fb08316a4e
Ilya Polovinkin <some@mail.com>
2023-04-11 11:37:39 +0300

    Fixes

--- UI/Implementation/QRCODE/QRCodeSetting.cs
+++ UI/Implementation/QRCODE/QRCodeSetting.cs
@@ -2,7 +2,7 @@ using UnityEngine;
 
 namespace Game.UI
 {
-    [CreateAssetMenu(menuName = "Game/UI Settings/QRCodeSetting", fileName = nameof(QRCodeSetting))]
+    [CreateAssetMenu(menuName = "Game/UI Settings/" + nameof(QRCodeSetting), fileName = nameof(QRCodeSetting))]
     public class QRCodeSetting : UISetting
     {
         public Vector2Int size;
